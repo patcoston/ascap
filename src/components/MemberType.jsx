4d11386428bbcb6e78d16eb6de092b3f660bf85b
Patrick Coston <patcoston@gmail.com>
2023-05-03 15:07:42 -0400

    Changed class to className, updated classes

--- src/components/MemberType.jsx
+++ src/components/MemberType.jsx
@@ -4,17 +4,23 @@ const MemberType = ({ type, nonRefundable, cost, description }) => {
       <h2>{type}</h2>
       <h1>
         <div>{cost}</div>
-        {nonRefundable ? <div>non-refundable</div> : null}
+        {nonRefundable ? (
+          <div className="requirement requirement-non-refundable">
+            non-refundable
+          </div>
+        ) : null}
       </h1>
-      <div class="description">{description}</div>
-      <div class="requirements">Requirements</div>
-      <div class="requirement requirement-legal-name">Legal Name</div>
-      <div class="requirement requirement-mailing-address">Mailing Address</div>
-      <div class="requirement requirement-valid-email-address">
+      <div className="description">{description}</div>
+      <div className="requirements">Requirements</div>
+      <div className="requirement requirement-legal-name">Legal Name</div>
+      <div className="requirement requirement-mailing-address">
+        Mailing Address
+      </div>
+      <div className="requirement requirement-valid-email-address">
         Valid Email Address
       </div>
-      <div class="requirement requirement-ssn-itin">SSN/ITIN</div>
-      <div class="requirement requirement-age-requirement">
+      <div className="requirement requirement-ssn-itin">SSN/ITIN</div>
+      <div className="requirement requirement-age-requirement">
         Must be 18 or older to apply online*
       </div>
     </div>
