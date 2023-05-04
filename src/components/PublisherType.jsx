c6ffb3baceebd7924dc926577115e80362b1a8fc
Patrick Coston <patcoston@gmail.com>
2023-05-04 18:03:00 -0400

    Moved Notes, Errors, Buttons from PublisherType back to SignupStep1

--- src/components/PublisherType.jsx
+++ src/components/PublisherType.jsx
@@ -1,13 +1,9 @@
 import '../sass/PublisherType.scss'
 import DropDownMenu from './DropDownMenu'
-import Button from './Button'
-import Error from './Error'
-import Note from './Note'
 
 const PublisherType = () => {
   return (
     <div className="publisher-type">
-      <Error msg="Please select your membership type." />
       <DropDownMenu
         label="Publisher Company Type"
         list={[
@@ -25,29 +21,6 @@ const PublisherType = () => {
           { uid: 8, type: 'OTHER' },
         ]}
       />
-      <Error msg="Please select your publisher company type." />
-      <Note>
-        *If you are under 18 years of age please{' '}
-        <a href={() => {}}>
-          download the Minor Application form (English &amp; Spanish)
-        </a>
-        .
-      </Note>
-      <Note>
-        ASCAP uses TINCheck and SmartyStreets to verify certain information
-        provided by you in connection with your application. Any information
-        processed by TINCheck and SmartyStreets shall be subject to the privacy
-        policies of <a href={() => {}}>TINCheck</a> and{' '}
-        <a href={() => {}}>SmartyStreets</a>.
-      </Note>
-      <div className="buttons">
-        <span className="first">
-          <Button type="cancel" labelText="Cancel" action={() => {}} />
-        </span>
-        <span className="second">
-          <Button type="continue" labelText="Continue" action={() => {}} />
-        </span>
-      </div>
     </div>
   )
 }
--- src/components/SignupStep1.jsx
+++ src/components/SignupStep1.jsx
@@ -1,12 +1,45 @@
 import '../sass/SignupStep1.scss'
 import MemberTypes from './MemberTypes'
 import PublisherType from './PublisherType'
+import Button from './Button'
+import Error from './Error'
+import Note from './Note'
 
 const SignupStep1 = () => {
   return (
     <div className="signup-step1">
-      <MemberTypes />
-      <PublisherType />
+      <div className="member-types-wrapper">
+        <MemberTypes />
+        <div className="error-wrapper">
+          <Error msg="Please select your membership type." />
+        </div>
+      </div>
+      <div className="publisher-type-wrapper">
+        <PublisherType />
+        <Error msg="Please select your publisher company type." />
+        <Note>
+          *If you are under 18 years of age please{' '}
+          <a href={() => {}}>
+            download the Minor Application form (English &amp; Spanish)
+          </a>
+          .
+        </Note>
+        <Note>
+          ASCAP uses TINCheck and SmartyStreets to verify certain information
+          provided by you in connection with your application. Any information
+          processed by TINCheck and SmartyStreets shall be subject to the
+          privacy policies of <a href={() => {}}>TINCheck</a> and{' '}
+          <a href={() => {}}>SmartyStreets</a>.
+        </Note>
+        <div className="buttons">
+          <span className="first">
+            <Button type="cancel" labelText="Cancel" action={() => {}} />
+          </span>
+          <span className="second">
+            <Button type="continue" labelText="Continue" action={() => {}} />
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
