09caeb12fe73802578539faa93f81b6717bfdee8
Patrick Coston <patcoston@gmail.com>
2023-05-04 17:43:53 -0400

    Moved publisher type into new component PublisherType

--- src/components/SignupStep1.jsx
+++ src/components/SignupStep1.jsx
@@ -1,57 +1,12 @@
 import '../sass/SignupStep1.scss'
 import MemberTypes from './MemberTypes'
-import DropDownMenu from './DropDownMenu'
-import Button from './Button'
-import Error from './Error'
-import Note from './Note'
+import PublisherType from './PublisherType'
 
 const SignupStep1 = () => {
   return (
     <div className="signup-step1">
       <MemberTypes />
-      <div className="publisher-type">
-        <Error msg="Please select your membership type." />
-        <DropDownMenu
-          label="Publisher Company Type"
-          list={[
-            {
-              uid: 0,
-              type: 'Individual / Sole proprietor or Single-member LLC',
-            },
-            { uid: 1, type: 'C Corporation' },
-            { uid: 2, type: 'S Corporation' },
-            { uid: 3, type: 'LLC - C Corporation' },
-            { uid: 4, type: 'LLC - S Corporation' },
-            { uid: 5, type: 'LLC - Partnership' },
-            { uid: 6, type: 'Partnership' },
-            { uid: 7, type: 'Trust / Estate' },
-            { uid: 8, type: 'OTHER' },
-          ]}
-        />
-        <Error msg="Please select your publisher company type." />
-        <Note>
-          *If you are under 18 years of age please{' '}
-          <a href={() => {}}>
-            download the Minor Application form (English &amp; Spanish)
-          </a>
-          .
-        </Note>
-        <Note>
-          ASCAP uses TINCheck and SmartyStreets to verify certain information
-          provided by you in connection with your application. Any information
-          processed by TINCheck and SmartyStreets shall be subject to the
-          privacy policies of <a href={() => {}}>TINCheck</a> and{' '}
-          <a href={() => {}}>SmartyStreets</a>.
-        </Note>
-        <div className="buttons">
-          <span className="first">
-            <Button type="cancel" labelText="Cancel" action={() => {}} />
-          </span>
-          <span className="second">
-            <Button type="continue" labelText="Continue" action={() => {}} />
-          </span>
-        </div>
-      </div>
+      <PublisherType />
     </div>
   )
 }
