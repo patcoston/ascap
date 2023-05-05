69458e5aa93591a2424eb2898ac36243f3fce0ff
Patrick Coston <patcoston@gmail.com>
2023-05-05 12:55:56 -0400

    Tried to make dropdown menu hide when click off it. Not working. TODO!

--- src/components/DropDownMenu.jsx
+++ src/components/DropDownMenu.jsx
@@ -1,14 +1,34 @@
 import '../sass/DropDownMenu.scss'
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 const DropDownMenu = ({ setType, isError, label, list }) => {
   const [open, setOpen] = useState(false)
   const [selection, setSelection] = useState('')
+  const menuRef = useRef(null)
 
   const handleClick = type => {
     setSelection(type)
     setType(type)
     setOpen(false)
+  }
+
+  // TODO: menu div is not getting focus on onBlur
+  // event is not firing to hide menu when you click off it
+  useEffect(() => {
+    console.log(open)
+    if (open) {
+      console.log('set focus')
+      menuRef.current.focus()
+    }
+  }, [open])
+
+  const hideMenu = () => {
+    console.log('hide menu')
+    setOpen(false)
+  }
+
+  const gettingFocus = () => {
+    console.log('got focus')
   }
 
   return (
@@ -21,7 +41,12 @@ const DropDownMenu = ({ setType, isError, label, list }) => {
         {selection}
       </button>
       {open && (
-        <div className="menu">
+        <div
+          className="menu"
+          ref={menuRef}
+          onFocus={() => gettingFocus()}
+          onBlur={() => hideMenu()}
+        >
           <ul className="list">
             {list.map(item => (
               <li
